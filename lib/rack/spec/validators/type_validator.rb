c3627372cf96a03c184d45659e070ca8b4cc02b5
Ryo Nakamura <r7kamura@gmail.com>
2014-02-22 20:55:53 +0900

    Add DSL to register new type-pattern mapping

--- lib/rack/spec/validators/type_validator.rb
+++ lib/rack/spec/validators/type_validator.rb
@@ -4,6 +4,20 @@ module Rack
   class Spec
     module Validators
       class TypeValidator < Base
+        class << self
+          def patterns
+            @patterns ||= Hash.new(//)
+          end
+
+          def register(name, pattern)
+            patterns[name] = pattern
+          end
+        end
+
+        register "float", /\A-?\d+(?:\.\d+)*\z/
+        register "integer", /\A-?\d+\z/
+        register "iso8601", ->(value) { Time.iso8601(value) rescue false }
+
         def validate!(env)
           value = extract_value(env) or return
           unless pattern === value
@@ -14,16 +28,7 @@ module Rack
         private
 
         def pattern
-          case @constraint
-          when "float"
-            /\A-?\d+(?:\.\d+)*\z/
-          when "integer"
-            /\A-?\d+\z/
-          when "iso8601"
-            ->(value) { Time.iso8601(value) rescue false }
-          else
-            //
-          end
+          self.class.patterns[@constraint]
         end
       end
     end
